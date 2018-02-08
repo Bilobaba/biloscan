bf67494e82fb453851c65c14d137c79a67eecdab
Duy DANG <tao.zen.duy@gmail.com>
2018-02-08 10:49:25 +0100

    titres ancienne/nouvelle page

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,5 +2,5 @@ class Member < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # :registerable, :trackable, :validatable
-  devise :database_authenticatable, :recoverable, :rememberable
+  devise :database_authenticatable, :recoverable, :registerable, :rememberable
 end
